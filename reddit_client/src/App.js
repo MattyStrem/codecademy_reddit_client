e9d8facf1c3857bcd2e9aa213b357d210f79ecae
Mathew Street <mathew.k.street@gmail.com>
2021-04-23 17:28:49 +0100

    css started

--- reddit_client/src/App.js
+++ reddit_client/src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 import Header from "./features/header/header"
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <main>
+        <div className="posts-container">
+          <div className="post">
+            <h3 className="post-header-container">Post 1</h3>
+            <img className="post-image-container"/>
+            <div className="post-comment-container">
+              <p>User</p>
+              <p>time ago</p>
+              <p>comments</p>
+            </div>
+          </div>
+        </div>
+      </main>
       
     </div>
   );
